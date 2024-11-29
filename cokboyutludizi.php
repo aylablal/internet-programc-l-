2a98af92c70d04df120394a4e8a9d3ec44dc90a5
aylablal <ayla.bilal@ogr.gelisim.edu.tr>
2024-11-29 18:22:20 +0300

    Add files via upload

---
+++ cokboyutludizi.php
@@ -0,0 +1,17 @@
+<?php
+
+//çok boyutlu dizi oluşturma
+
+/*$ogrenci=array();
+$ogrenci[0]=array("ayla","bulgaristan");
+$ogrenci[1]=array("zeynep","edirne");
+$ogrenci[2]=array("arda","sivas");
+echo "<pre>";
+print_r($ogrenci);
+echo "</pre>";*/
+
+
+//
+
+
+?>
